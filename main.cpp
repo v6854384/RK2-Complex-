c03053c77e5b554d2b9627bc861f1d089c4f3ccf
v6854384 <32385043+v6854384@users.noreply.github.com>
2018-04-20 14:10:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,4 +2,43 @@
 int main() {  
   Complex c1{};
   Complex c2{3};
+  Complex c3{1, 4};
   
+  cout << c1 << endl;
+  cout << c2 << endl;
+  cout << c3 << endl;
+  
+  Complex c4{c3};
+  Complex c5;
+  c5 = c4;
+  
+  cout << c4 << endl;
+  cout << c5 << endl;
+  
+  cout << c2 + c5 << endl;
+  cout << c2 - c5 << endl;
+  cout << c2 * c5 << endl;
+  cout << c2 / c5 << endl;
+  
+  cout << boolalpha;
+  cout << (c2 != c5) << endl;
+  cout << (c3 == c5) << endl;
+  
+  cout << static_cast<double>(c5) << endl;
+  cout << c5[0] << "," << c5[1] << endl;
+  
+  cin >> c1;
+  ofstream output{ "complex.bin" };
+  
+  ifstream input{ "complex.bin" };
+  input >> c1;
+  
+  auto c6 = Complex::from_string("(1,2)");
+  cout << c6 << endl;
+  
+  c1.swap(c6); 
+  cout << c1 << endl;
+  
+  cout << !c1 << endl;
+  cout << c5[2] << endl;
+}
